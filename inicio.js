06b801d15eaef67c22ca755c07b06f3ddc07e3c1
torinoemi <torinogastonemi@gmail.com>
2023-10-06 20:14:01 -0300

    iniciando proyecto

--- inicio.js
+++ inicio.js
@@ -3,8 +3,6 @@ import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 
 export default function HomeScreen() {
-    return (
-        
-    )
+   
 
 }
---
+++ registro.js
@@ -0,0 +1,8 @@
+import { View, text } from 'react-native'
+import React from 'react' 
+import { SafeAreaView } from 'react-native-safe-area-context'
+
+export default function HomeScreen() {
+   
+
+}
